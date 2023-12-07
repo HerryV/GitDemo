ec93fb572321151c270ca2f194425ceddf74c726
HerryV <153160993+HerryV@users.noreply.github.com>
2023-12-07 15:41:04 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-         System.out.println("Hello jnc!");
-        System.out.println("Hello Good!");
-        System.out.println("Hello jnc!");
+         
     }
 }
